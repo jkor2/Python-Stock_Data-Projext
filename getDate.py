a3378d73da8774f6b4088ea59b044b9f79865f89
Jake Korobellis <jkorobellis@gmail.com>
2023-10-31 16:24:51 -0400

    Seperated logic

--- getDate.py
+++ getDate.py
@@ -6,8 +6,14 @@ def get_current_date():
     Gets current date and returns proper format
     """
     current_date = datetime.now()
+    return current_date
+
+
+def seven_days_ago():
+    """
+    Gets date from 7 days ago and return the proper format
+    """
+
+    current_date = datetime.now()
     seven_days = current_date - timedelta(days=7)
-    seven_days, current_date
-
-
-print(get_current_date())
+    return seven_days
