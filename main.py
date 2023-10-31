f8000fbe4b77f7ec8f391c27a7793201dfeda6ae
Jake Korobellis <jkorobellis@gmail.com>
2023-10-31 16:28:37 -0400

    INitalized members

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import getDate
+
+
 class Main:
-    pass
+    def __init__(self, stock="SPY", current_date=getDate.get_current_date(), past_date=getDate.seven_days_ago()):
+        self._stock = stock
+        self._current_date = current_date
+        self._past_date = past_date
