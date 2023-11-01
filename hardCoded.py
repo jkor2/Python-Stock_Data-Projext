27e0c7f8e0d71016d387bcdfa37668e61f1f0be7
Jake Korobellis <jkorobellis@gmail.com>
2023-11-01 00:01:58 -0400

    Stock Info

---
+++ hardCoded.py
@@ -0,0 +1,2 @@
+stock_info_keys = ['currency', 'dayHigh', 'dayLow', 'exchange', 'fiftyDayAverage', 'lastPrice', 'lastVolume', 'marketCap', 'open', 'previousClose', 'quoteType',
+                   'regularMarketPreviousClose', 'shares', 'tenDayAverageVolume', 'threeMonthAverageVolume', 'timezone', 'twoHundredDayAverage', 'yearChange', 'yearHigh', 'yearLow']
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import getDate
 import yfinance as yf
 
+# Internal Imports
+import hardCoded
+
 
 class Main:
     def __init__(self, stock="SPY", current_date=getDate.get_current_date(), past_date=getDate.seven_days_ago()):
@@ -11,6 +14,7 @@ class Main:
         self._chart_data = []
         self._ml_data = []
         self._daily_time_frame = "5m"
+        self._stock_info = {}
 
     def fetch_data_range(self):
         """
@@ -32,12 +36,36 @@ class Main:
             self._stock, interval=self._daily_time_frame, period='1d')
         self._process_and_set(data)
 
+    def fetch_stock_information(self):
+        info = yf.Ticker(self._stock).basic_info
+        # Process Info
+        info_object = {}
+
+        # Cleanse data - not all stocks have values for keys
+        for i in hardCoded.stock_info_keys:
+            # Validate there is data present in Key
+            try:
+                if info[i]:
+                    info_object[i] = info[i]
+                else:
+                    pass
+            except:
+                pass
+
+        self._stock_info = info_object
+
     def get_active_data(self):
         """
         Public Method
         Gets the hashed data
         """
         return self._active_data
+
+    def get_stock_info(self):
+        """
+        Return private stock info data memeber
+        """
+        return self._stock_info
 
     def _process_and_set(self, data):
         """
@@ -51,8 +79,10 @@ class Main:
 
 main = Main()
 main.fetch_data_range()
-data = main.get_active_data()
-print(data)
-main.fetch_current_day_data()
-data = main.get_active_data()
-print(data)
+# data = main.get_active_data()
+# print(data)
+# main.fetch_current_day_data()
+# data = main.get_active_data()
+# print(data)
+main.fetch_stock_information()
+print(main.get_stock_info())
