69d6df60ef808c108f34017e447bbf30dfb03728
Jake Korobellis <jkorobellis@gmail.com>
2023-10-31 23:38:05 -0400

    Private Method Added to Process data from API calls

--- main.py
+++ main.py
@@ -8,31 +8,35 @@ class Main:
         self._current_date = current_date
         self._past_date = past_date
         self._active_data = []
+        self._chart_data = []
+        self._ml_data = []
 
     def load_data(self):
         """
+        Public Method  
         Returns default data
         """
-
         data = yf.download(self._stock, start=self._past_date,
                            end=self._current_date)
-
-        data_list_of_dicts = data.reset_index().to_dict(orient='records')
-        self.set_active_data(data_list_of_dicts)
-
-    def set_active_data(self, data):
-        """
-        Updates the hashed data
-        """
-        self._hashed_data = data
-        return True
+        self._process_and_set(data)
 
     def get_active_data(self):
         """
+        Public Method
         Gets the hashed data
         """
-        return self._hashed_data
+        return self._active_data
+
+    def _process_and_set(self, data):
+        """
+        Will be used to process data and properly store it
+        for active data, chart data, and ML data 
+        """
+        self._active_data = data
+        self._chart_data = data
+        self._ml_data = data
 
 
 main = Main()
 main.load_data()
+data = main.get_active_data()
